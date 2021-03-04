7773e865d79715c146d5e02f212894e14e500b06
MakerFace <chenqi@imudges.com>
2021-03-04 22:00:28 +0800

    upgrad read from yaml and remove extra shells

--- utils/read_config.py
+++ utils/read_config.py
@@ -1,1 +1,26 @@
+#!/usr/bin/env python
+
 # TODO read yaml
+import yaml
+
+yml = 'config/config.yml'
+
+def open_yaml():
+    with open(yml) as file:
+        res = yaml.safe_load(file)
+        return res
+
+def get_vscode():
+    return open_yaml()['vscode']
+
+def get_location():
+    return open_yaml()['location']
+
+def get_themes():
+    return open_yaml()['themes']
+
+def get_gnome():
+    return open_yaml()['gnome']
+
+def get_all():
+    return open_yaml()
